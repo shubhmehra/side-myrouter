0042c8980cbb165eabf90f3d7009ddcb3b2ce6d7
Shubham Mehra <mehrashubham18@gmail.com>
2021-07-28 16:12:55 +0530

    Basic Routing Example for blog

--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Route, Switch, Link, BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import User from "./User";
+import Visit from "./Visit";
+import NotFound from "./NotFound";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const routing = (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/user">User</Link>
+        </li>
+        <li>
+          <Link to="/visit">Visit</Link>
+        </li>
+      </ul>
+    </div>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/user" component={User} />
+      <Route path="/visit" component={Visit} />
+      <Route component={NotFound} />
+    </Switch>
+  </Router>
 );
+
+ReactDOM.render(routing, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
